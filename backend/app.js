c83aa4bcf29c173293aab9901754318bc9708b93
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-11-25 19:51:00 +0800

    feat(backend): Add cors package to setup cors middleware

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import cors from 'cors'
 
 const app = express();
 const port = 3000;
 
 
 app.use(express.json())
-
+app.use(cors({origin: "http://127.0.0.1:5500"}))
 let todos = [];
 
 // Get All Todos
